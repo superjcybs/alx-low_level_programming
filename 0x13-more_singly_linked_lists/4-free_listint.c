30f81873924b78f5ca9ec9bc5d96ab5403fd7516
Superjcybs <jerrybabatunde1@gmail.com>
2024-07-06 21:02:09 -0700

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "list.h"
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to the head of listint_t list to work on
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
