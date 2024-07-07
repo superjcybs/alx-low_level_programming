7143042e544b1beb5f479da7281adc18891a847f
Superjcybs <jerrybabatunde1@gmail.com>
2024-07-06 21:15:56 -0700

    frees a listsint_t list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a listint_t list
+ * @head: a pointer to the first element on the list
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
+
